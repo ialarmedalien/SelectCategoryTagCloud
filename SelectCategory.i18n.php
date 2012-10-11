85b52a2700a23f20be321c7edcadfd55fe31607d
girlwithglasses <amelia.ireland@gmail.com>
2012-10-11 13:48:22 -0700

    Initial commit of SCTC

---
+++ SelectCategory.i18n.php
@@ -0,0 +1,53 @@
+<?php
+
+# Internationalisation file for the SelectCategory extension for MediaWiki
+/* SelectCategoryTagCloud Mediawiki Extension
+ *
+ * @author Andreas Rindler (mediawiki at jenandi dot com)
+ * @credits Leon Weber <leon.weber@leonweber.de> & Manuel Schneider <manuel.schneider@wikimedia.ch>, Daniel Friesen http://wiki-tools.com
+ * @licence GNU General Public Licence 2.0 or later
+ * @description Adds a category selection tag cloud to the edit and upload pages and enables a Google Suggest like completion of categories enter by the user
+ *
+*/
+$messages = array();
+
+$messages['en'] = array(
+'selectcategory-title' => 'CATEGORY SELECTION',
+'selectcategory-subtitle' => 'Please enter a category for this article or select from the list of popular categories.',
+'selectcategory-boxlabel' => 'Categories',
+'selectcategory-taglabel' => 'Popular Categories',
+'selectcategory-tooltip' => 'Click here to add or remove this category!',
+'selectcategory-userexplanation' => 'A category is a <b>grouping of related articles</b>. You can <b>add this wiki article</b> to one or many of the below categories by:<br>1. Type in a new category name into the input box. <br>2. Type in the name of an existing category into the input box and select from the suggestions below it. <br>3. Select one of the categories from Popular Categories.<br><br> <b>For example</b>, if you are creating a new Solution Offering article, then you should add the category Solution Offerings to the article. Users will then be able to find all Solution Offerings (including the one that you just created) by clicking on the Solution Offerings category link on the bottom of the article page. <br><br>For <b>more information</b> on Categories, click on <b>Help</b> on the left of the page.',
+);
+
+# @author Leon Weber <leon.weber@leonweber.de> & Manuel Schneider <manuel.schneider@wikimedia.ch>
+# translation done by Manuel Schneider <manuel.schneider@wikimedia.ch>
+$messages['de'] = array(
+	'selectcategory-title' => 'Kategorien auswählen',
+	'selectcategory-subtitle' => 'Shift-Maus um mehrere nachfolgende Einträge zu (de-)selektieren, Strg-Maus um einzelne Einträge zu (de-)selektieren.',
+);
+
+# translation done by Marius Engler <marius.engler@coop.ch>
+$messages['fr'] = array(
+'selectcategory-title' => 'Choix de catégories',
+'selectcategory-subtitle' => 'Shift+Souris afin de (de)sélectionner plusieurs catégories d\'affilées, Ctrl+Souris afin de (de)sélectionner des catégories.',
+);
+
+# translation done by Helix84
+$messages['sk'] = array(
+'selectcategory-title' => 'Vybrať kategórie',
+'selectcategory-subtitle' => 'Shift+kliknutie označí súvislú skupinu položiek, ctrl+kliknutie označí jednotlivé nesúvislé položky.',
+);
+
+# translation done by Marius Engler <marius.engler@coop.ch>
+$messages['it'] = array(
+'selectcategory-title' => 'Selezionare le categorie',
+'selectcategory-subtitle' => 'Shift-mouse per selezionare le entrate attigue multiple, Ctrl-mouse per selezionare le entrate non-attigue.',
+);
+
+# translation done by Manuel Schneider <manuel.schneider@wikimedia.ch>
+$messages['gsw'] = array(
+'selectcategory-title' => 'Kategori ussueche',
+'selectcategory-subtitle' => 'Gross-Muus go verschiedeni Iiträg hintrenander go uswähle, Strg-Muus go einzelni Iiträg go uswähle',
+);
+
